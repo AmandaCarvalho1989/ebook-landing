9da197e2737df5f79f2fca47c32034e8931e60aa
AmandaCarvalho1989 <amandamvcarvalho1989@gmail.com>
2024-03-03 12:40:15 -0300

    feat: create content section

---
+++ src/constants/chapters.js
@@ -0,0 +1,54 @@
+
+import bookMark from '../images/svg/book-mark.svg'
+import file from '../images/svg/file.svg'
+import imagesGroup from '../images/svg/images-group.svg'
+import paintBrush from '../images/svg/paint-brush.svg'
+import target from '../images/svg/target.svg'
+import textarea from '../images/svg/textarea.svg'
+import textbox from '../images/svg/textbox.svg'
+import toggleRight from '../images/svg/toggle-right.svg'
+
+const chapters = [
+  {
+    title: 'Introduction',
+    description: " In this book, we will provide you with a step-by-step guide to creating landing pages that are visually appealing and highly effective in marketing goals.",
+    icon: bookMark,
+  },
+  {
+    title: 'Colors',
+    description: "Colors play a crucial role in design. They can evoke emotions, communicate messages, and influence our perceptions of products or brands.",
+    icon: paintBrush,
+  },
+  {
+    title: 'Typhography',
+    description: " Good typography can make text more readable, enhance the visual appeal of a design, and even convey a specific mood or tone.",
+    icon: textarea,
+  },
+  {
+    title: 'Icons',
+    description: "Icons are a powerful design element that can communicate complex ideas in a simple and visually appealing way.",
+    icon: file,
+  },
+  {
+    title: 'Images',
+    description: "Images are a key element of design that can greatly enhance the visual appeal and effectiveness of a design.",
+    icon: imagesGroup,
+  },
+  {
+    title: 'Buttons',
+    description: "They provide a clear call-to-action for users, guiding them towards the next step in the user journey, also  are important functional and aesthetic purpose.",
+    icon: toggleRight,
+  },
+  {
+    title: 'Forms',
+    description: "Forms are an essential element of design that allow users to provide information or feedback in a structured and organized way.",
+    icon: textbox,
+  },
+  {
+    title: 'Tips',
+    description: "We also separate tips on copy, storytelling and sales strategies, practical challenges and questionnaires to raise your level and ensure your learning.",
+    icon: target
+  },
+]
+
+export { chapters }
