6310d897fba1c63e6f58134d538b3054b9864b61
AmandaCarvalho1989 <amandamvcarvalho1989@gmail.com>
2024-03-04 20:30:08 -0300

    feat: create FAQ section

---
+++ src/constants/questions.js
@@ -0,0 +1,24 @@
+const questions = [
+  {
+    question:"If I don't like it, do I get my money back?",
+    answer:"This material has a 7-day guarantee, that is, if within 7 days you access the ebook, get to know it, and still think that the material is not for you, just request a refund of your money. Just send an email to refundlandingebook@gmail.com"
+  },
+  {
+    question:"What are the paying ways?",
+    answer:"This material has a 7-day guarantee, that is, if within 7 days you access the ebook, get to know it, and still think that the material is not for you, just request a refund of your money. Just send an email to refundlandingebook@gmail.com"
+  },
+  {
+    question:"Where do I access the ebook after purchase?",
+    answer:"This material has a 7-day guarantee, that is, if within 7 days you access the ebook, get to know it, and still think that the material is not for you, just request a refund of your money. Just send an email to refundlandingebook@gmail.com"
+  },
+  {
+    question:"Is the ebook complete or will it receive updates?",
+    answer:"This material has a 7-day guarantee, that is, if within 7 days you access the ebook, get to know it, and still think that the material is not for you, just request a refund of your money. Just send an email to refundlandingebook@gmail.com"
+  },
+  {
+    question:"What if I have questions not listed here?",
+    answer:"This material has a 7-day guarantee, that is, if within 7 days you access the ebook, get to know it, and still think that the material is not for you, just request a refund of your money. Just send an email to refundlandingebook@gmail.com"
+  },
+]
+
+export {questions}
