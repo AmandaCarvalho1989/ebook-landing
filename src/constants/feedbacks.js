21bf806f87a17edaa3373efe06faf1cf49ce464a
AmandaCarvalho1989 <amandamvcarvalho1989@gmail.com>
2024-03-04 10:52:15 -0300

    feat: create feedbacks section

---
+++ src/constants/feedbacks.js
@@ -0,0 +1,36 @@
+
+import person1 from "../images/person-1.png";
+import person2 from "../images/person-2.png";
+import person3 from "../images/person-3.png";
+
+const feedbacks = [
+  {
+    name: 'Robert Fox',
+    profession: 'Marketing Coordinator',
+    description: "I recently read [book title], and I have to say, it's one of the most practical and comprehensive guides to landing page optimization that I've come across.",
+    rating: 5,
+    image: person1,
+    date: '16/02/2023'
+  },
+  {
+    name: 'Wade Warren',
+    profession: 'Web Designer',
+    description: "I highly recommend  this book to anyone looking to improve their landing pages. It's a must-read for who wants to create landing pages that really work.",
+    rating: 5,
+    image: person2,
+    date: '05/03/2023'
+  },
+
+  {
+    name: 'Esther Howard',
+    profession: 'UI/UX Designer',
+    description: "The author's writing style is clear, concise, and engaging, making it easy to follow along and apply the insights to your own landing pages.",
+    rating: 4,
+    image: person3,
+    date: '20/03/2023'
+  },
+
+]
+
+export { feedbacks }
+
