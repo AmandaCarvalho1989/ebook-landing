9caad3ef4abd9e30a94b5570fa9444aa538f0ff4
AmandaCarvalho1989 <amandamvcarvalho1989@gmail.com>
2024-03-05 09:53:55 -0300

    feat: update questions component

--- src/constants/questions.js
+++ src/constants/questions.js
@@ -1,23 +1,23 @@
 const questions = [
   {
     question:"If I don't like it, do I get my money back?",
-    answer:"This material has a 7-day guarantee, that is, if within 7 days you access the ebook, get to know it, and still think that the material is not for you, just request a refund of your money. Just send an email to refundlandingebook@gmail.com"
+    answer:"This material has a 7-day guarantee, that is, if within 7 days you access the ebook, get to know it, and still think that the material is not for you, just request a refund of your money. Just send an email to refundlandingebook@gmail.com",
   },
   {
     question:"What are the paying ways?",
-    answer:"This material has a 7-day guarantee, that is, if within 7 days you access the ebook, get to know it, and still think that the material is not for you, just request a refund of your money. Just send an email to refundlandingebook@gmail.com"
+    answer:"What are the paying ways?",
   },
   {
     question:"Where do I access the ebook after purchase?",
-    answer:"This material has a 7-day guarantee, that is, if within 7 days you access the ebook, get to know it, and still think that the material is not for you, just request a refund of your money. Just send an email to refundlandingebook@gmail.com"
+    answer:"Where do I access the ebook after purchase?",
   },
   {
     question:"Is the ebook complete or will it receive updates?",
-    answer:"This material has a 7-day guarantee, that is, if within 7 days you access the ebook, get to know it, and still think that the material is not for you, just request a refund of your money. Just send an email to refundlandingebook@gmail.com"
+    answer:"Is the ebook complete or will it receive updates?",
   },
   {
     question:"What if I have questions not listed here?",
-    answer:"This material has a 7-day guarantee, that is, if within 7 days you access the ebook, get to know it, and still think that the material is not for you, just request a refund of your money. Just send an email to refundlandingebook@gmail.com"
+    answer:"What if I have questions not listed here?",
   },
 ]
 
