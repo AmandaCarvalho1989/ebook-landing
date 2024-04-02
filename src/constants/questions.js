4d2da8ec12b10f8ec30a29ec6044c9989ae7ff61
AmandaCarvalho1989 <amandamvcarvalho1989@gmail.com>
2024-04-02 10:36:44 -0300

    feat: add scroll transition and questions's answsers

--- src/constants/questions.js
+++ src/constants/questions.js
@@ -4,20 +4,20 @@ const questions = [
     answer:"This material has a 7-day guarantee, that is, if within 7 days you access the ebook, get to know it, and still think that the material is not for you, just request a refund of your money. Just send an email to refundlandingebook@gmail.com",
   },
   {
-    question:"What are the paying ways?",
-    answer:"What are the paying ways?",
+    question:"What are the paying methods?",
+    answer:"We accept various payment methods including credit/debit cards, PayPal, and bank transfers. You can choose the one that suits you best during the checkout process.",
   },
   {
     question:"Where do I access the ebook after purchase?",
-    answer:"Where do I access the ebook after purchase?",
+    answer:"After purchase, you will receive an email with instructions on how to access the ebook. Alternatively, you can log in to your account on our website and access it from there",
   },
   {
     question:"Is the ebook complete or will it receive updates?",
-    answer:"Is the ebook complete or will it receive updates?",
+    answer:"The ebook is currently complete. However, we continuously strive to improve our content, so occasional updates may be released. As a purchaser, you will have access to any updates free of charge",
   },
   {
     question:"What if I have questions not listed here?",
-    answer:"What if I have questions not listed here?",
+    answer:"If you have any questions that are not addressed here, please feel free to reach out to our customer support team. You can contact us via email at support@yourwebsite.com or through our contact form on the website. We're here to help!",
   },
 ]
 
